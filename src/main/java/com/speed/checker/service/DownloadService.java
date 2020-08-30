fac5e2279932d7d803ae3e3c53530dcb18c0bb65
Rob Squire <squirer7492@gmail.com>
2020-08-30 12:48:22 +0100

    Adding initial folder structure

---
+++ src/main/java/com/speed/checker/service/DownloadService.java
@@ -0,0 +1,4 @@
+package com.speed.checker.service;
+
+public interface DownloadService {
+}
---
+++ src/main/java/com/speed/checker/service/DownloadServiceImpl.java
@@ -0,0 +1,7 @@
+package com.speed.checker.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DownloadServiceImpl implements DownloadService{
+}

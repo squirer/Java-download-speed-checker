fac919de26eb3f43210e5dc592c5b091153f1abd
Rob Squire <squirer7492@gmail.com>
2020-08-30 12:52:53 +0100

    Added config for properties later and also a welcome controller working

--- src/main/java/com/speed/checker/controller/DownloadRestController.java
+++ src/main/java/com/speed/checker/controller/DownloadRestController.java
@@ -1,9 +1,13 @@
 package com.speed.checker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DownloadRestController {
 
-    
+    @GetMapping("/welcome")
+    public String testWelcomeEndpoint(){
+        return "Application accessible!";
+    }
 }

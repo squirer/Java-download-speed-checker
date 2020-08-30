45889821d9434891c404e4deca988486a6513735
Rob Squire <squirer7492@gmail.com>
2020-08-30 14:02:24 +0100

    Some helper scripts to deploy to a Linode server

---
+++ src/main/deploy/deployToServer/deployDownloadData.sh
@@ -0,0 +1,4 @@
+
+echo 'Deploying download data'
+
+scp src/main/deploy/downloadData/20MB.zip ubuntu@178.79.162.105:/home/ubuntu/downloadTestData
---
+++ src/main/deploy/deployToServer/deployLinode.sh
@@ -0,0 +1,4 @@
+
+echo 'Deploying to Linode server now'
+
+scp target/Java-download-speed-checker-bin.zip ubuntu@178.79.162.105:/home/ubuntu

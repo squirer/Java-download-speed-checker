5e00da4b0d2ff42a0b5e531ae85d247ccd62ea56
Rob Squire <squirer7492@gmail.com>
2020-08-30 13:35:46 +0100

    Adding a sample 20MB zip which will be used for the test

--- src/main/java/com/speed/checker/config/ApplicationProperties.java
+++ src/main/java/com/speed/checker/config/ApplicationProperties.java
@@ -1,7 +1,30 @@
 package com.speed.checker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationProperties {
+
+    @Value("${download.file.location}")
+    private String downloadFileLocation;
+
+    @Value("${byte.buffer.download.size}")
+    private int byteBufferDownloadSize;
+
+    public String getDownloadFileLocation() {
+        return downloadFileLocation;
+    }
+
+    public void setDownloadFileLocation(String downloadFileLocation) {
+        this.downloadFileLocation = downloadFileLocation;
+    }
+
+    public int getByteBufferDownloadSize() {
+        return byteBufferDownloadSize;
+    }
+
+    public void setByteBufferDownloadSize(int byteBufferDownloadSize) {
+        this.byteBufferDownloadSize = byteBufferDownloadSize;
+    }
 }
